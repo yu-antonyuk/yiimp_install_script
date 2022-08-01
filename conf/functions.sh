a6045e9dfc218022965067835f508814c8206bd0
afinieldev <afinieldev@hotmail.com>
2022-08-01 22:51:50 +0200

    update functions.sh

--- conf/functions.sh
+++ conf/functions.sh
@@ -109,17 +109,15 @@ function term_art {
 	echo
 	echo -e "$CYAN  --------------------------------------------------------------------------- 	  		$COL_RESET"
 	echo -e "$YELLOW  Welcome to the Yiimp Installer Script , Fork By Afiniel!								$COL_RESET"
-	echo -e "$GREEN Version: v0.4 																			$COL_RESET"
+	echo -e "$GREEN  Version: v0.4 																			$COL_RESET"
 	echo -e "$CYAN  --------------------------------------------------------------------------- 	  		$COL_RESET"
 	echo -e "$YELLOW  This script will install all the dependencies and will install Yiimp.					$COL_RESET"
 	echo -e "$YELLOW  It will also install a MySQL database and a Web server.								$COL_RESET"
-	echo -e "$YELLOW  MariaDB is used for the database: MariaDB Version:	v								$COL_RESET"
+	echo -e "$YELLOW  MariaDB is used for the database.														$COL_RESET"
 	echo -e "$YELLOW  Nginx is used for the Web server, PHP 7.3 is also installed.							$COL_RESET"
 	echo -e "$CYAN  --------------------------------------------------------------------------- 	  		$COL_RESET"
-	echo -e "$YELLOW  Yiimp Installer Script Fork By Afiniel https://github.com/afiniel/yiimp_install_script $COL_RESET"
-	echo -e "$CYAN  --------------------------------------------------------------------------- 	  		$COL_RESET"
-
 	echo
+
 }
 
 function hide_output {
