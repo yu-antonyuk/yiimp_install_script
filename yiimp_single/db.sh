fb9bb6b7b70cd422d3608518e9c2dfcfcd1adb51
afiniel <44875524+afiniel@users.noreply.github.com>
2022-10-31 21:38:31 +0100

    Update db.sh
    Add SQL Import on: 2022-10-29-blocks_effort.sql

--- yiimp_single/db.sh
+++ yiimp_single/db.sh
@@ -112,6 +112,7 @@ sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force <2018-09-22-wo
 sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force <2019-03-coins_thepool_life.sql
 sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force <2020-06-03-blocks.sql
 sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force <2022-10-14-shares_solo.sql
+sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force <2022-10-29-blocks_effort.sql
 echo -e "$YELLOW => Datebase import $GREEN complete$COL_RESET"
 
 echo -e "$YELLOW => Tweaking MariaDB for better performance$COL_RESET"
