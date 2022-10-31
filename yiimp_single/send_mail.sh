cafab59983325003dd936f18c64828f95f893215
afiniel <44875524+afiniel@users.noreply.github.com>
2022-10-31 18:32:37 +0100

    Update send_mail.sh

--- yiimp_single/send_mail.sh
+++ yiimp_single/send_mail.sh
@@ -39,7 +39,8 @@ sudo sed -i '/# myhostname =/i myhostname = localhost' /etc/postfix/main.cf
 sudo systemctl restart postfix
 whoami=$(whoami)
 
-
+cd $HOME/etc/
+sudo touch aliases
 sudo sed -i '/postmaster:    root/a root:          '${SupportEmail}'' /etc/aliases
 sudo sed -i '/root:/a '$whoami':     '${SupportEmail}'' /etc/aliases
 sudo newaliases
