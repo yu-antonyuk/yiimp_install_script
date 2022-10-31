ae8a3ede6de3a11335407c3eaac4b1774ab51a4d
afiniel <44875524+afiniel@users.noreply.github.com>
2022-10-31 17:07:52 +0100

    Update daemon.sh

--- yiimp_single/daemon.sh
+++ yiimp_single/daemon.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-#####################################################
-# Created by Afiniel for Yiimpool use...
-#####################################################
+##########################################
+# Created by Afiniel for Yiimpool use... #
+##########################################
 
 source /etc/functions.sh
 source /etc/yiimpool.conf
@@ -32,7 +32,7 @@ apt_install build-essential libtool autotools-dev \
 
 sudo mkdir -p $STORAGE_ROOT/yiimp/yiimp_setup/tmp
 cd $STORAGE_ROOT/yiimp/yiimp_setup/tmp
-echo -e "$GREEN Additional System Files Completed$COL_RESET"
+echo -e "$GREEN => Additional System Files Completed $COL_RESET"
 
 echo -e "$YELLOW => Building Berkeley 4.8, this may take several minutes$COL_RESET"
 sudo mkdir -p $STORAGE_ROOT/berkeley/db4/
@@ -40,7 +40,7 @@ hide_output sudo wget 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.g
 hide_output sudo tar -xzvf db-4.8.30.NC.tar.gz
 cd db-4.8.30.NC/build_unix/
 hide_output sudo ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$STORAGE_ROOT/berkeley/db4/
-hide_output sudo make install -j8
+hide_output sudo make -j$((`nproc`+1))
 cd $STORAGE_ROOT/yiimp/yiimp_setup/tmp
 sudo rm -r db-4.8.30.NC.tar.gz db-4.8.30.NC
 echo -e "$GREEN Berkeley 4.8 Completed$COL_RESET"
@@ -51,7 +51,7 @@ hide_output sudo wget 'http://download.oracle.com/berkeley-db/db-5.1.29.tar.gz'
 hide_output sudo tar -xzvf db-5.1.29.tar.gz
 cd db-5.1.29/build_unix/
 hide_output sudo ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$STORAGE_ROOT/berkeley/db5/
-hide_output sudo make install -j8
+hide_output sudo make -j$((`nproc`+1))
 cd $STORAGE_ROOT/yiimp/yiimp_setup/tmp
 sudo rm -r db-5.1.29.tar.gz db-5.1.29
 echo -e "$GREEN Berkeley 5.1 Completed$COL_RESET"
@@ -62,7 +62,7 @@ hide_output sudo wget 'http://anduin.linuxfromscratch.org/BLFS/bdb/db-5.3.28.tar
 hide_output sudo tar -xzvf db-5.3.28.tar.gz
 cd db-5.3.28/build_unix/
 hide_output sudo ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$STORAGE_ROOT/berkeley/db5.3/
-hide_output sudo make install -j8
+hide_output sudo make -j$((`nproc`+1))
 cd $STORAGE_ROOT/yiimp/yiimp_setup/tmp
 sudo rm -r db-5.3.28.tar.gz db-5.3.28
 echo -e "$GREEN Berkeley 5.3 Completed$COL_RESET"
@@ -73,8 +73,8 @@ hide_output sudo wget https://www.openssl.org/source/old/1.0.2/openssl-1.0.2g.ta
 hide_output sudo tar -xf openssl-1.0.2g.tar.gz
 cd openssl-1.0.2g
 hide_output sudo ./config --prefix=$STORAGE_ROOT/openssl --openssldir=$STORAGE_ROOT/openssl shared zlib
-hide_output sudo make
-hide_output sudo make install
+hide_output sudo make -j$((`nproc`+1))
+hide_output sudo make install -j$((`nproc`+1))
 cd $STORAGE_ROOT/yiimp/yiimp_setup/tmp
 sudo rm -r openssl-1.0.2g.tar.gz openssl-1.0.2g
 echo -e "$GREEN OpenSSL 1.0.2g Completed$COL_RESET"
@@ -85,7 +85,7 @@ hide_output sudo wget 'https://github.com/codablock/bls-signatures/archive/v2018
 hide_output sudo unzip v20181101.zip
 cd bls-signatures-20181101
 hide_output sudo cmake .
-hide_output sudo make install
+hide_output sudo make install -j$((`nproc`+1))
 cd $STORAGE_ROOT/yiimp/yiimp_setup/tmp
 sudo rm -r v20181101.zip bls-signatures-20181101
 echo -e "$GREEN bls-signatures Completed$COL_RESET"
@@ -93,23 +93,23 @@ echo -e "$GREEN bls-signatures Completed$COL_RESET"
 echo -e "$YELLOW => Building blocknotify.sh$COL_RESET"
 if [[ ("$wireguard" == "true") ]]; then
   source $STORAGE_ROOT/yiimp/.wireguard.conf
-  echo '#####################################################
-  # Created by Afiniel for Yiimpool use...
-  #####################################################
+  echo '#####################################
+  # Created by Afiniel for Yiimpool use...  #
+  ###########################################
   #!/bin/bash
   blocknotify '""''"${DBInternalIP}"''""':$1 $2 $3' | sudo -E tee /usr/bin/blocknotify.sh >/dev/null 2>&1
   sudo chmod +x /usr/bin/blocknotify.sh
 else
-  echo '#####################################################
-  # Created by Afiniel for Yiimpool use...
-  #####################################################
+  echo '#####################################
+  # Created by Afiniel for Yiimpool use...  #
+  ###########################################
   #!/bin/bash
   blocknotify 127.0.0.1:$1 $2 $3' | sudo -E tee /usr/bin/blocknotify.sh >/dev/null 2>&1
   sudo chmod +x /usr/bin/blocknotify.sh
 fi
 
 echo
-echo -e "$GREEN Daemon setup completed$COL_RESET"
+echo -e "$GREEN Daemon setup completed $COL_RESET"
 
 set +eu +o pipefail
 cd $HOME/yiimp_install_script/yiimp_single
