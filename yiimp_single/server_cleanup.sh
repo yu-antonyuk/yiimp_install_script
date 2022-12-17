2cf49d98e0356952a427a804d39ac4af0c1c5c83
afiniel <44875524+afiniel@users.noreply.github.com>
2022-12-17 16:14:44 +0100

    Update server_cleanup.sh

--- yiimp_single/server_cleanup.sh
+++ yiimp_single/server_cleanup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-#####################################################
-# Created by Afiniel for Yiimpool use...
-#####################################################
+#########################################
+# Created by Afiniel for Yiimpool use...#
+#########################################
 
 source /etc/functions.sh
 source $STORAGE_ROOT/yiimp/.yiimp.conf
@@ -13,7 +13,7 @@ term_art
 echo -e "$MAGENTA    <----------------------------->$COL_RESET"
 echo -e "$YELLOW     <-- Starting Server Cleanup -->$COL_RESET"
 echo -e "$MAGENTA    <----------------------------->$COL_RESET"
-
+echo
 echo -e "$YELLOW => Installing cron screens to crontab <= $COL_RESET"
 
 (
@@ -38,6 +38,7 @@ fi
 sudo cp -r first_boot.sh $STORAGE_ROOT/yiimp/
 
 echo -e "$GREEN Crontab system complete$COL_RESET"
+echo
 echo -e "$YELLOW => Creating YiiMP Screens startup script <= $COL_RESET"
 
 echo '#!/usr/bin/env bash
@@ -71,6 +72,7 @@ screen -dmS debug tail -f $LOG_DIR/debug.log
 ' | sudo -E tee $STORAGE_ROOT/yiimp/starts/screens.start.sh >/dev/null 2>&1
 sudo chmod +x $STORAGE_ROOT/yiimp/starts/screens.start.sh
 
+echo
 echo -e "$YELLOW => Creating Stratum screens start script <= $COL_RESET"
 
 echo '#!/usr/bin/env bash
@@ -156,7 +158,7 @@ STRATUM_DIR=$STORAGE_ROOT/yiimp/site/stratum
 
 echo "source /etc/yiimpool.conf" | hide_output tee -a ~/.bashrc
 echo "source $STORAGE_ROOT/yiimp/.prescreens.start.conf" | hide_output tee -a ~/.bashrc
-echo -e "$GREEN YiiMP Screens added$COL_RESET"
+echo -e "$YELLOW YiiMP Screens$GREEN Added$COL_RESET"
 
 sudo rm -r $STORAGE_ROOT/yiimp/yiimp_setup
 
