2e16ba62057e21cf02f1c1e2b5a32e993cbe01b7
afiniel <44875524+afiniel@users.noreply.github.com>
2023-01-17 19:54:27 +0000

    type

--- daemon_builder/conf/daemonbuilder.sh
+++ daemon_builder/conf/daemonbuilder.sh
@@ -9,7 +9,7 @@ source $STORAGE_ROOT/yiimp/.yiimp.conf
 absolutepath=absolutepathserver
 installtoserver=installpath
 daemonname=daemonnameserver
-# path_stratum="{$STORAGE_ROOT}/yiimp/stratum"
+path_stratum=$STORAGE_ROOT/yiimp/site/stratum/config
 
 ESC_SEQ="\x1b["
 COL_RESET=$ESC_SEQ"39;49;00m"
