d18585e9c3a7929435cf908486b64085ced74e74
afiniel <44875524+afiniel@users.noreply.github.com>
2023-01-31 14:56:26 +0000

    Update db.sh

--- yiimp_single/db.sh
+++ yiimp_single/db.sh
@@ -26,32 +26,44 @@ echo -e "$MAGENTA    <----------------------------->$COL_RESET"
 echo -e "$YELLOW     <-- Installing MariaDB 10.4 -->$COL_RESET"
 echo -e "$MAGENTA    <----------------------------->$COL_RESET"
 
+# Define MariaDB version
 MARIADB_VERSION='10.4'
+
+# Set MariaDB root password for installation
 sudo debconf-set-selections <<<"maria-db-$MARIADB_VERSION mysql-server/root_password password $DBRootPassword"
 sudo debconf-set-selections <<<"maria-db-$MARIADB_VERSION mysql-server/root_password_again password $DBRootPassword"
+
+# Install MariaDB
 apt_install mariadb-server mariadb-client
+
+# Display completion message
 echo -e "$GREEN => MariaDB build complete <= $COL_RESET"
 echo
+
+# Display message for creating DB users
 echo -e "$MAGENTA => Creating DB users for YiiMP <= $COL_RESET"
 
-if [[ ("$wireguard" == "false") ]]; then
+# Check if wireguard variable is set to false
+if [ "$wireguard" = "false" ]; then
+  # Define SQL statements
   Q1="CREATE DATABASE IF NOT EXISTS ${YiiMPDBName};"
   Q2="GRANT ALL ON ${YiiMPDBName}.* TO '${YiiMPPanelName}'@'localhost' IDENTIFIED BY '$PanelUserDBPassword';"
   Q3="GRANT ALL ON ${YiiMPDBName}.* TO '${StratumDBUser}'@'localhost' IDENTIFIED BY '$StratumUserDBPassword';"
   Q4="FLUSH PRIVILEGES;"
   SQL="${Q1}${Q2}${Q3}${Q4}"
+  # Run SQL statements
   sudo mysql -u root -p"${DBRootPassword}" -e "$SQL"
 
 else
+  # Define SQL statements
   Q1="CREATE DATABASE IF NOT EXISTS ${YiiMPDBName};"
   Q2="GRANT ALL ON ${YiiMPDBName}.* TO '${YiiMPPanelName}'@'${DBInternalIP}' IDENTIFIED BY '$PanelUserDBPassword';"
   Q3="GRANT ALL ON ${YiiMPDBName}.* TO '${StratumDBUser}'@'${DBInternalIP}' IDENTIFIED BY '$StratumUserDBPassword';"
   Q4="FLUSH PRIVILEGES;"
   SQL="${Q1}${Q2}${Q3}${Q4}"
+  # Run SQL statements
   sudo mysql -u root -p"${DBRootPassword}" -e "$SQL"
 fi
-
-echo -e "$GREEN => Database creation complete <= $COL_RESET"
 
 echo -e "$MAGENTA => Creating my.cnf <= $COL_RESET"
 
@@ -92,50 +104,62 @@ sudo chmod 0600 $STORAGE_ROOT/yiimp/.my.cnf
 
 echo -e "$YELLOW => Importing YiiMP Default database values <= $COL_RESET"
 cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/sql
+
 # import SQL dump
 sudo zcat 2020-11-10-yaamp.sql.gz | sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}"
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2016-04-24-market_history.sql
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2016-04-27-settings.sql
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2016-05-11-coins.sql
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2016-05-15-benchmarks.sql
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2016-05-23-bookmarks.sql
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2016-06-01-notifications.sql
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2016-06-04-bench_chips.sql
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2016-11-23-coins.sql
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2017-02-05-benchmarks.sql
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2017-03-31-earnings_index.sql
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2017-05-accounts_case_swaptime.sql
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2017-06-payouts_coinid_memo.sql
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2017-09-notifications.sql
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2017-10-bookmarks.sql
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2017-11-segwit.sql
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2018-01-stratums_ports.sql
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2018-02-coins_getinfo.sql
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2018-09-22-workers.sql
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2019-03-coins_thepool_life.sql
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2020-06-03-blocks.sql
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2022-10-14-shares_solo.sql
-sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < 2022-10-29-blocks_effort.sql
+
+SQL_FILES=(
+  2016-04-24-market_history.sql
+  2016-04-27-settings.sql
+  2016-05-11-coins.sql
+  2016-05-15-benchmarks.sql
+  2016-05-23-bookmarks.sql
+  2016-06-01-notifications.sql
+  2016-06-04-bench_chips.sql
+  2016-11-23-coins.sql
+  2017-02-05-benchmarks.sql
+  2017-03-31-earnings_index.sql
+  2017-05-accounts_case_swaptime.sql
+  2017-06-payouts_coinid_memo.sql
+  2017-09-notifications.sql
+  2017-10-bookmarks.sql
+  2017-11-segwit.sql
+  2018-01-stratums_ports.sql
+  2018-02-coins_getinfo.sql
+  2018-09-22-workers.sql
+  2019-03-coins_thepool_life.sql
+  2020-06-03-blocks.sql
+  2022-10-14-shares_solo.sql
+  2022-10-29-blocks_effort.sql
+)
+
+for file in "${SQL_FILES[@]}"; do
+  sudo mysql -u root -p"${DBRootPassword}" "${YiiMPDBName}" --force < "$file"
+done
+
 echo -e "$YELLOW <-- Datebase import $GREEN complete -->$COL_RESET"
 
 echo
 echo -e "$YELLOW => Tweaking MariaDB for better performance <= $COL_RESET"
-if [[ ("$wireguard" == "false") ]]; then
-  sudo sed -i '/max_connections/c\max_connections         = 800' /etc/mysql/my.cnf
-  sudo sed -i '/thread_cache_size/c\thread_cache_size       = 512' /etc/mysql/my.cnf
-  sudo sed -i '/tmp_table_size/c\tmp_table_size          = 128M' /etc/mysql/my.cnf
-  sudo sed -i '/max_heap_table_size/c\max_heap_table_size     = 128M' /etc/mysql/my.cnf
-  sudo sed -i '/wait_timeout/c\wait_timeout            = 60' /etc/mysql/my.cnf
-  sudo sed -i '/max_allowed_packet/c\max_allowed_packet      = 64M' /etc/mysql/my.cnf
-else
-  sudo sed -i '/max_connections/c\max_connections         = 800' /etc/mysql/my.cnf
-  sudo sed -i '/thread_cache_size/c\thread_cache_size       = 512' /etc/mysql/my.cnf
-  sudo sed -i '/tmp_table_size/c\tmp_table_size          = 128M' /etc/mysql/my.cnf
-  sudo sed -i '/max_heap_table_size/c\max_heap_table_size     = 128M' /etc/mysql/my.cnf
-  sudo sed -i '/wait_timeout/c\wait_timeout            = 60' /etc/mysql/my.cnf
-  sudo sed -i '/max_allowed_packet/c\max_allowed_packet      = 64M' /etc/mysql/my.cnf
-  sudo sed -i 's/#bind-address=0.0.0.0/bind-address='${DBInternalIP}'/g' /etc/mysql/my.cnf
+
+# Define MariaDB configuration changes
+config_changes=(
+  'max_connections = 800'
+  'thread_cache_size = 512'
+  'tmp_table_size = 128M'
+  'max_heap_table_size = 128M'
+  'wait_timeout = 60'
+  'max_allowed_packet = 64M'
+)
+
+# Add bind-address if wireguard is false
+if [[ "$wireguard" == "false" ]]; then
+  config_changes+=('bind-address='"$DBInternalIP")
 fi
+
+# Apply changes to MariaDB configuration
+sudo sed -i "/^\(\w\+\)\s\+\w\+/c\\1 $(echo "${config_changes[@]}" | tr ' ' '\n')" /etc/mysql/my.cnf
+
 
 # Restart MariaDB
 restart_service mysql
