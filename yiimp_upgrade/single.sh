f8b57c07d59725810e039b5d3c3c0c45ef86cc69
afiniel <44875524+afiniel@users.noreply.github.com>
2023-03-08 16:43:33 +0100

    Yiimp stratum update option update.

--- yiimp_upgrade/single.sh
+++ yiimp_upgrade/single.sh
@@ -8,22 +8,30 @@ source $STORAGE_ROOT/yiimp/.yiimp.conf
 
 if [[ ! -e '$STORAGE_ROOT/yiimp/yiimp_setup/yiimp' ]]; then
 sudo rm -r $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
-hide_output sudo git clone ${YiiMPRepo} $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
+sudo git clone ${YiiMPRepo} $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
 else
-hide_output sudo git clone ${YiiMPRepo} $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
+sudo git clone ${YiiMPRepo} $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
 fi
 
-echo -e "$YELLOW Upgrading stratum... $COL_RESET"
-cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/stratum/iniparser
-hide_output make -j$((`nproc`+1))
+echo -e "$CYAN Updating Stratum... $COL_RESET"
+# cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/stratum/iniparser
+# sudo make -j$((`nproc`+1))
 
-cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/stratum
-if [[ ("$AutoExchange" == "y" || "$AutoExchange" == "Y" || "$AutoExchange" == "yes" || "$AutoExchange" == "Yes" || "$AutoExchange" == "YES") ]]; then
-sudo sed -i 's/CFLAGS += -DNO_EXCHANGE/#CFLAGS += -DNO_EXCHANGE/' $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/stratum/Makefile
+# Compil Stratum
+cd /home/crypto-data/yiimp/yiimp_setup/yiimp/stratum
+
+sudo git submodule init && sudo git submodule update
+sudo make -C algos
+sudo make -C sha3
+sudo make -C iniparser
+cd secp256k1 && sudo chmod +x autogen.sh && sudo ./autogen.sh && sudo ./configure --enable-experimental --enable-module-ecdh --with-bignum=no --enable-endomorphism && sudo make -j$((`nproc`+1))
+if [[ ("$AutoExchange" == "y" || "$AutoExchange" == "Y") ]]; then
+sudo sed -i 's/CFLAGS += -DNO_EXCHANGE/#CFLAGS += -DNO_EXCHANGE/' /home/crypto-data/yiimp/yiimp_setup/yiimp/stratum/Makefile
 fi
-hide_output make -j$((`nproc`+1))
 
-cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/stratum
+cd /home/crypto-data/yiimp/yiimp_setup/yiimp/stratum
+sudo make -j$((`nproc`+1))
+
 sudo mv stratum $STORAGE_ROOT/yiimp/site/stratum
 
 cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/web/yaamp/core/functions/
