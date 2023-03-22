63fc4b7196a7d06ab757aa0cfa9ea031b1060bee
afiniel <44875524+afiniel@users.noreply.github.com>
2023-03-22 18:27:10 +0100

    Update install.sh

--- daemon_builder/install.sh
+++ daemon_builder/install.sh
@@ -5,12 +5,12 @@
 # web: https://afiniel.xyz
 # Program:
 #   Install Daemon Coin on Ubuntu 18.04/20.04
-#   v0.7.9 (2023-01-04)
+#   v0.8.0 (2023-01-04)
 #
 ################################################################################
 
 if [ -z "${TAG}" ]; then
-	TAG=v0.7.9
+	TAG=v0.8.0
 fi
 
 clear
