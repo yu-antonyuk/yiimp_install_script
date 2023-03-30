043b4c4e00bc62e9ce21a7d1fd94e02205de1c58
afiniel <44875524+afiniel@users.noreply.github.com>
2023-03-30 12:53:35 +0200

    Set tag version

--- daemon_builder/install.sh
+++ daemon_builder/install.sh
@@ -5,12 +5,12 @@
 # web: https://afiniel.xyz
 # Program:
 #   Install Daemon Coin on Ubuntu 18.04/20.04
-#   v0.8.2 (2023-01-04)
+#   v0.8.3 (2023-01-04)
 #
 ################################################################################
 
 if [ -z "${TAG}" ]; then
-	TAG=v0.8.2
+	TAG=v0.8.3
 fi
 
 clear
--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@
 #########################################################
 
 if [ -z "${TAG}" ]; then
-	TAG=v0.8.2
+	TAG=v0.8.3
 fi
 
 echo 'VERSION='"${TAG}"'' | sudo -E tee /etc/yiimpoolversion.conf >/dev/null 2>&1
