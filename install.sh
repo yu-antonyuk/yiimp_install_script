c80e1a5c89da30faf68eac047f6b3119ccb20076
afiniel <44875524+afiniel@users.noreply.github.com>
2023-03-30 08:13:47 +0200

    tag since DAemonBuilder fix Forgot..

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@
 #########################################################
 
 if [ -z "${TAG}" ]; then
-	TAG=v0.8.0
+	TAG=v0.8.2
 fi
 
 echo 'VERSION='"${TAG}"'' | sudo -E tee /etc/yiimpoolversion.conf >/dev/null 2>&1
