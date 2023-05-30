e7b8367d4436105afb91d2de33891d65cfd1872b
afiniel <44875524+afiniel@users.noreply.github.com>
2023-05-30 17:54:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@
 #########################################################
 
 if [ -z "${TAG}" ]; then
-	TAG=v0.8.6
+	TAG=v0.8.7
 fi
 
 echo 'VERSION='"${TAG}"'' | sudo -E tee /etc/yiimpoolversion.conf >/dev/null 2>&1
