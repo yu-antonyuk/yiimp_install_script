a5e76fdb2cf23f2176d7cd3c4dcf451623860a32
afiniel <44875524+afiniel@users.noreply.github.com>
2023-05-30 13:29:03 +0200

    Update berkeley.sh

--- daemon_builder/berkeley.sh
+++ daemon_builder/berkeley.sh
@@ -133,7 +133,7 @@ echo
 
 echo -e "$MAGENTA => Building bls-signatures$GREEN <= $COL_RESET"
 cd $STORAGE_ROOT/yiimp/yiimp_setup/tmp
-hide_output sudo wget 'https://github.com/codablock/bls-signatures/archive/v20181101.zip'
+hide_output sudo wget 'https://github.com/dashpay/bls-signatures/archive/v20181101.zip'
 hide_output sudo unzip v20181101.zip
 cd bls-signatures-20181101
 hide_output sudo cmake .
