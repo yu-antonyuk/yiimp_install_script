a7e100595cf51a3c5f6f3c1e816314435c4a8e58
afiniel <44875524+afiniel@users.noreply.github.com>
2023-06-04 18:18:35 +0200

    Update preflight.sh

--- install/preflight.sh
+++ install/preflight.sh
@@ -9,8 +9,15 @@
 if [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/18\.04\.[0-9]/18.04/' `" == "Ubuntu 18.04 LTS" ]; then
   DISTRO=18
   sudo chmod g-w /etc /etc/default /usr
-else [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/16\.04\.[0-9]/16.04/' `" != "Ubuntu 16.04 LTS" ];
+elif [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/20\.04\.[0-9]/20.04/' `" == "Ubuntu 20.04 LTS" ]; then
+  DISTRO=20
+  sudo chmod g-w /etc /etc/default /usr
+elif [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/16\.04\.[0-9]/16.04/' `" == "Ubuntu 16.04 LTS" ]; then
   DISTRO=16
+  sudo chmod g-w /etc /etc/default /usr
+else
+  echo "This script only supports Ubuntu 16.04 LTS, 18.04 LTS, and 20.04 LTS."
+  exit
 fi
 
 TOTAL_PHYSICAL_MEM=$(head -n 1 /proc/meminfo | awk '{print $2}')
@@ -46,18 +53,19 @@ if
 then
   echo "Adding a swap file to the system..."
 
-  # Allocate and activate the swap file. Allocate in 1KB chuncks
-  # doing it in one go, could fail on low memory systems
+  # Allocate and activate the swap file. Allocate in 1KB chunks
+  # doing it in one go could fail on low memory systems
   sudo fallocate -l 3G /swapfile
-    if [ -e /swapfile ]; then
-      sudo chmod 600 /swapfile
-      hide_output sudo mkswap /swapfile
-      sudo swapon /swapfile
-      echo "vm.swappiness=10" >> sudo /etc/sysctl.conf
-    fi
-# Check if swap is mounted then activate on boot
+  if [ -e /swapfile ]; then
+    sudo chmod 600 /swapfile
+    sudo mkswap /swapfile
+    sudo swapon /swapfile
+    echo "vm.swappiness=10" | sudo tee -a /etc/sysctl.conf
+  fi
+
+  # Check if swap is mounted then activate on boot
   if swapon -s | grep -q "\/swapfile"; then
-    echo "/swapfile  none swap sw 0  0" >> sudo /etc/fstab
+    echo "/swapfile  none swap sw 0  0" | sudo tee -a /etc/fstab
   else
     echo "ERROR: Swap allocation failed"
   fi
@@ -66,7 +74,7 @@ fi
 ARCHITECTURE=$(uname -m)
 if [ "$ARCHITECTURE" != "x86_64" ]; then
   if [ -z "$ARM" ]; then
-    echo "Yiimpool Installer only supports x86_64 and will not work on any other architecture, like ARM or 32 bit OS."
+    echo "Yiimpool Installer only supports x86_64 and will not work on any other architecture, like ARM or 32-bit OS."
     echo "Your architecture is $ARCHITECTURE"
     exit
   fi
