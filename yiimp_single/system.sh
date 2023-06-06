dc87160de0e679a21187377e829853c505d1cc0d
afiniel <44875524+afiniel@users.noreply.github.com>
2023-06-06 19:23:28 +0200

    Update system.sh

--- yiimp_single/system.sh
+++ yiimp_single/system.sh
@@ -36,31 +36,31 @@ if [ ! -f /etc/timezone ]; then
 fi
 echo
 
-sudo apt-get install -y software-properties-common build-essential
+hide_output sudo apt-get install -y software-properties-common build-essential
 
 # CertBot
 if [[ ("$DISTRO" == "16") ]]; then
 	echo
 	echo -e "$MAGENTA => Installing CertBot PPA <= $COL_RESET"
-	sudo add-apt-repository -y ppa:certbot/certbot
-	sudo apt-get update
+	hide_output sudo add-apt-repository -y ppa:certbot/certbot
+	hide_output sudo apt-get update
 	echo -e "$GREEN => Complete$COL_RESET"
 fi
 
 if [[ ("$DISTRO" == "18") ]]; then
 	echo
 	echo -e "$MAGENTA => Installing CertBot PPA <= $COL_RESET"
-	sudo add-apt-repository -y ppa:certbot/certbot
-	sudo apt-get update
+	hide_output sudo add-apt-repository -y ppa:certbot/certbot
+	hide_output sudo apt-get update
 	echo -e "$GREEN => Complete$COL_RESET"
 fi
 
 if [[ ("$DISTRO" == "20") ]]; then
 	echo
 	echo -e "$MAGENTA => Installing CertBot PPA <= $COL_RESET"
-	sudo apt install -y snapd
-	sudo snap install core; sudo snap refresh core
-	sudo snap install --classic certbot
+	hide_output sudo apt install -y snapd
+	hide_output sudo snap install core; sudo snap refresh core
+	hide_output sudo snap install --classic certbot
 	sudo ln -s /snap/bin/certbot /usr/bin/certbot
 
 	echo -e "$GREEN => Complete$COL_RESET"
@@ -68,7 +68,7 @@ fi
 # MariaDB
 echo
 echo -e "$MAGENTA =>  Installing MariaDB <= $COL_RESET"
-sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+hide_output sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 
 if [[ "$DISTRO" == "18" ]]; then
     sudo add-apt-repository 'deb [arch=amd64,arm64,i386,ppc64el] http://mirror.one.com/mariadb/repo/10.4/ubuntu bionic main' >/dev/null 2>&1
@@ -85,8 +85,8 @@ fi
 echo -e "$GREEN => Complete$COL_RESET"
 
 # Upgrade System Files
-sudo apt-get update
-sudo apt-get upgrade -y
+hide_output sudo apt-get update
+hide_output sudo apt-get upgrade -y
 
 if [ ! -f /boot/grub/menu.lst ]; then
 	apt_get_quiet upgrade
@@ -103,7 +103,7 @@ apt_get_quiet autoremove
 
 echo
 echo -e "$MAGENTA => Installing Base system packages <= $COL_RESET"
-sudo apt-get install -y python3 python3-dev python3-pip \
+apt_install python3 python3-dev python3-pip \
 	wget curl git sudo coreutils bc \
 	haveged pollinate unzip \
 	unattended-upgrades cron ntp fail2ban screen rsyslog lolcat nginx
@@ -112,8 +112,8 @@ sudo apt-get install -y python3 python3-dev python3-pip \
 echo -e "$GREEN => Complete$COL_RESET"
 echo
 echo -e "$YELLOW => Initializing system random number generator <= $COL_RESET"
-dd if=/dev/random of=/dev/urandom bs=1 count=32 2>/dev/null
-sudo pollinate -q -r
+hide_output dd if=/dev/random of=/dev/urandom bs=1 count=32 2>/dev/null
+hide_output sudo pollinate -q -r
 echo -e "$GREEN => Complete$COL_RESET"
 
 echo
@@ -121,7 +121,7 @@ echo -e "$YELLOW => Initializing UFW Firewall <= $COL_RESET"
 set +eu +o pipefail
 if [ -z "${DISABLE_FIREWALL:-}" ]; then
 	# Install `ufw` which provides a simple firewall configuration.
-	sudo apt-get install -y ufw
+	hide_output sudo apt-get install -y ufw
 	echo
 	echo -e "$YELLOW => Allow incoming connections to SSH <= $COL_RESET"
 	echo
@@ -163,53 +163,53 @@ if [ -z "${DISABLE_FIREWALL:-}" ]; then
 		fi
 	fi
 
-	sudo ufw --force enable
+	hide_output sudo ufw --force enable
 fi
 set -eu -o pipefail
 echo
 echo -e "$MAGENTA =>  Installing YiiMP Required system packages <= $COL_RESET"
 if [ -f /usr/sbin/apache2 ]; then
 	echo Removing apache...
-	sudo apt-get -y purge apache2 apache2-*
-	sudo apt-get -y --purge autoremove
+	hide_output sudo apt-get -y purge apache2 apache2-*
+	hide_output sudo apt-get -y --purge autoremove
 fi
 
-sudo apt-get update
+hide_output sudo apt-get update
 
 # Installing Installing php7.3
 echo
 echo -e "$CYAN => Installing php7.3 : $COL_RESET"
 sleep 3
 
-sudo apt -y update
+hide_output sudo apt -y update
 
 if [ ! -f /etc/apt/sources.list.d/ondrej-php-bionic.list ]; then
 	sudo add-apt-repository -y ppa:ondrej/php
 fi
-sudo apt -y update
+hide_output sudo apt -y update
 
-# TODO Add  app_install back.
+
 if [[ ("$DISTRO" == "16") ]]; then
-	sudo apt-get install -y php7.3-fpm php7.3-opcache php7.3 php7.3-common php7.3-gd php7.3-mysql php7.3-imap php7.3-cli
-	sudo apt-get install -y php7.3-cgi php-pear php-auth imagemagick libruby php7.3-curl php7.3-intl php7.3-pspell mcrypt
-	sudo apt-get install -y php7.3-recode php7.3-sqlite3 php7.3-tidy php7.3-xmlrpc php7.3-xsl memcached php-memcache php-imagick
-	sudo apt-get install -y php-gettext php7.3-zip php7.3-mbstring php7.3-memcache php7.3-memcached
+	apt_install php7.3-fpm php7.3-opcache php7.3 php7.3-common php7.3-gd php7.3-mysql php7.3-imap php7.3-cli
+	apt_install php7.3-cgi php-pear php-auth imagemagick libruby php7.3-curl php7.3-intl php7.3-pspell mcrypt
+	apt_install php7.3-recode php7.3-sqlite3 php7.3-tidy php7.3-xmlrpc php7.3-xsl memcached php-memcache php-imagick
+	apt_install php-gettext php7.3-zip php7.3-mbstring php7.3-memcache php7.3-memcached
 else
-	sudo apt-get install -y php7.3-fpm php7.3-opcache php7.3 php7.3-common php7.3-gd php7.3-mysql php7.3-imap php7.3-cli
-	sudo apt-get install -y php7.3-cgi php-pear imagemagick libruby php7.3-curl php7.3-intl php7.3-pspell mcrypt
-	sudo apt-get install -y php7.3-recode php7.3-sqlite3 php7.3-tidy php7.3-xmlrpc php7.3-xsl memcached php-memcache php-imagick php7.3-zip
-	sudo apt-get install -y php7.3-mbstring libpsl-dev libnghttp2-dev php7.3-memcache php7.3-memcached php-curl
-	sudo apt-get install -y php-mbstring php-zip php-gd php-json
+	apt_install php7.3-fpm php7.3-opcache php7.3 php7.3-common php7.3-gd php7.3-mysql php7.3-imap php7.3-cli
+	apt_install php7.3-cgi php-pear imagemagick libruby php7.3-curl php7.3-intl php7.3-pspell mcrypt
+	apt_install php7.3-recode php7.3-sqlite3 php7.3-tidy php7.3-xmlrpc php7.3-xsl memcached php-memcache php-imagick php7.3-zip
+	apt_install php7.3-mbstring libpsl-dev libnghttp2-dev php7.3-memcache php7.3-memcached php-curl
+	apt_install php-mbstring php-zip php-gd php-json
 	sudo phpenmod mbstring
 fi
 if [[ ("$DISTRO" == "18") ]]; then
-	sudo apt-get install -y php-gettext
+	apt_install php-gettext
 fi
 if [[ ("$DISTRO" == "20") ]]; then
-	sudo apt install -y php8.2-fpm php8.2-opcache php8.2 php8.2-common php8.2-gd php8.2-mysql php8.2-imap php8.2-cli
-	sudo apt install -y php8.2-cgi php8.2-curl php8.2-intl php8.2-pspell
-	sudo apt install -y php8.2-sqlite3 php8.2-tidy php8.2-xmlrpc php8.2-xsl php8.2-zip
-	sudo apt install -y php8.2-mbstring php8.2-memcache php8.2-memcached
+	apt_install php8.2-fpm php8.2-opcache php8.2 php8.2-common php8.2-gd php8.2-mysql php8.2-imap php8.2-cli
+	apt_install php8.2-cgi php8.2-curl php8.2-intl php8.2-pspell
+	apt_install php8.2-sqlite3 php8.2-tidy php8.2-xmlrpc php8.2-xsl php8.2-zip
+	apt_install php8.2-mbstring php8.2-memcache php8.2-memcached
 	# sleep 2
 	#  sudo systemctl start php8.2-fpm
 	# sudo systemctl status php8.2-fpm | sed -n "1,3p"
@@ -232,14 +232,14 @@ echo
 
 echo
 echo -e "$CYAN =>  Clone Yiimp Repo <= $COL_RESET"
-sudo git clone ${YiiMPRepo} $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
+hide_output sudo git clone ${YiiMPRepo} $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
 if [[ ("$CoinPort" == "yes") ]]; then
 	cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
 	sudo git fetch
 	sudo git checkout dev >/dev/null 2>&1
 fi
 
-hide_output service nginx restart
+hide_output sudo service nginx restart
 
 set +eu +o pipefail
 cd $HOME/yiimp_install_script/yiimp_single
