eaa4c55ebd6a56112860e3d02fb601c4d7fa7c8c
yu-antonyuk <83603871+yu-antonyuk@users.noreply.github.com>
2023-08-23 23:35:40 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@
 #########################################################
 
 if [ -z "${TAG}" ]; then
-	TAG=v0.9.1
+	TAG=v0.9.0
 fi
 
 echo 'VERSION='"${TAG}"'' | sudo -E tee /etc/yiimpoolversion.conf >/dev/null 2>&1
